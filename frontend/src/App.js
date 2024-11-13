96b0195749e136808f34bf48a42c395022f4b86e
exuvious <phannhatminh58@gmail.com>
2024-11-13 12:26:03 +0700

    add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
-import Dashboard from './Components/pages/dashboard';
+import Dashboard from './Components/pages/Dashboard';
 import AccountCreation from './Components/pages/AccountCreation.jsx';
+import Messages from './Components/pages/Messages';
+import Profile from './Components/pages/Profile';
+import Order from './Components/pages/Order';
+import Role from './Components/pages/Role';
+import Navbar from './Components/Appbar/Navbar';
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== '/' && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login  />} />
+        <Route path="/accountCreate" element={<AccountCreation />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/role" element={<Role />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-    <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/accountCreate" element ={<AccountCreation/>} />
-    </Routes>
-</Router>
+      <AppContent />
+    </Router>
   );
 }
 
