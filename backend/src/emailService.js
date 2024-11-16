43d95fbfb2352d6dc8b5e2f830385450171e3823
haizuoc <7210hai@gmail.com>
2024-11-16 21:47:27 +0700

    mailService update

---
+++ backend/src/emailService.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// Set up email transporter using Gmail's SMTP server
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// Send OTP email
+const sendOtpEmail = (email, otp) => {
+  console.log(`Preparing to send OTP (${otp}) to email: ${email}`);
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: 'Your OTP Code', 
+    text: `Your OTP code is: ${otp}`,
+  };
+
+  return transporter.sendMail(mailOptions)
+    .then(() => {
+      console.log(`Email sent to ${email} with OTP: ${otp}`); 
+    })
+    .catch((error) => {
+      console.error("Error in sendOtpEmail:", error);
+      throw error;
+    });
+};
+
+module.exports = { sendOtpEmail };
